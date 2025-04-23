a8b95eec37c22d038000f19046b8eaceb14ad35c
Bear-software <linner_952@mail.ru>
2025-04-23 18:11:54 +0200

    alfa v.

---
+++ src/lib/shared/data/navigationNgo.ts
@@ -0,0 +1,12 @@
+const navigation = [
+	{
+		text: 'Strona WWW',
+		link: 'https://fundacjawip.wordpress.com/'
+	},
+	// {
+	// 	text: 'Facebook',
+	// 	link: 'https://fundacjawip.wordpress.com/'
+	// }
+];
+
+export default navigation;
---
+++ src/lib/shared/data/partnersList.ts
@@ -0,0 +1,31 @@
+const partners = [
+	{
+		img: '/images/partners/logo-foundation-gray.svg',
+		title: 'Fundacja Wolność i Pokój',
+		link: 'https://fundacjawip.wordpress.com/'
+	},
+
+	// {
+	// 	img: '/images/partners/',
+	// 	title: 'Lorem Ipsum jest',
+	// 	link: '/'
+	// },
+	// {
+	// 	img: '/images/partners/',
+	// 	title: 'Lorem Ipsum jest',
+	// 	link: '/'
+	// },
+	// {
+	// 	img: '/images/partners/',
+	// 	title: 'Lorem Ipsum jest',
+	// 	link: '/'
+	// },
+	// {
+	// 	img: '/images/partners/',
+	// 	title: 'Lorem Ipsum jest',
+	// 	link: '/'
+	// }
+];
+
+
+export default partners;
