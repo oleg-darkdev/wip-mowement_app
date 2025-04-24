443ad090806b1d35aa3c7a56ab78c5b9fe3dee11
Bear-software <linner_952@mail.ru>
2025-04-24 23:02:58 +0200

    add slide animation

---
+++ src/lib/entities/app/CongratCard.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { selectedMenu, balance } from '$stores/app';
+
+	import { slide } from 'svelte/transition';
+</script>
+
+<div transition:slide class="h-full w-full">
+	<div
+		class="margin-bottom margin-small flex h-full flex-col content-center items-center justify-center"
+	>
+		<h3
+			class="tab_heading mb-10 flex flex-col content-center items-center justify-center text-center"
+		>
+			Gratulacje <br /> z okazji
+
+			<div class="tab_list-item w-full">
+				<div class="flex w-full flex-row justify-between">
+					<div
+						class="tab_item mx-auto"
+						style="
+							background-color: var(--black);
+							color: var(--white);"
+					>
+						<button
+							class=""
+							onclick={() => {
+								$balance += 6;
+								$selectedMenu = 0;
+							}}
+						>
+							<span>6</span><img
+								src="/images/dove-icon_gray.svg"
+								class="-ml-2 h-20 w-14 pb-2"
+								alt=""
+							/>
+						</button>
+					</div>
+				</div>
+			</div>
+		</h3>
+
+		<div class="flex w-full max-w-sm flex-row justify-between">
+			<button onclick={() => ($selectedMenu = 0)} class="button is-secondary"
+				><div class="button-text">Menu</div>
+				<div class="button-background"></div>
+			</button>
+
+			<button onclick={() => ($selectedMenu = 2)} class="button is-secondary"
+				><div class="button-text">Sklepik</div>
+				<div class="button-background"></div>
+			</button>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.tab_item {
+		border: 2px solid var(--black);
+	}
+</style>
--- src/lib/widgets/app/Footer.svelte
+++ src/lib/widgets/app/Footer.svelte
@@ -2,11 +2,17 @@
 	// import {  } from '$shared';
 	// import {  } from '$widgets';
 	// import {  } from '$entities'
+
+	// import { slide } from 'svelte/transition'; transition:slide
 </script>
 
-<footer class="background-color-gray fixed bottom-0 left-0  w-full">
+<footer  class="background-color-gray fixed bottom-0 left-0 w-full">
 	<div class="mx-auto w-full max-w-4xl pb-6">
-		<slot />
+		<div class="mx-auto w-full max-w-lg">
+			<div class="mx-auto flex justify-center gap-x-4">
+				<slot />
+			</div>
+		</div>
 	</div>
 </footer>
 
