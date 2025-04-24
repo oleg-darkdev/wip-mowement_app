7766c295ba0e8642233e50698ec5ac3ad470dee0
Bear-software <linner_952@mail.ru>
2025-04-24 23:01:14 +0200

    refactoring play menu

--- src/lib/widgets/app/WelcomeScreen.svelte
+++ src/lib/widgets/app/WelcomeScreen.svelte
@@ -1,149 +1,43 @@
 <script lang="ts">
 	import { slide } from 'svelte/transition';
 
-	import { selectedMenu, selectedAppMode, welcomeScreen, selectedPlayMenu } from '$stores/app';
-	import { appScreensMenu } from '$sharedData';
-
-	// 	const selectedLevelData = derived(stepInstruction, ($stepInstruction) => {
-	// 	console.log($stepInstruction);
-	// 	if ($stepInstruction < END_DELEGATION_LEVELS_INSTRUCTION)
-	// 		return ($stepInstruction = $stepInstruction - START_DELEGATION_LEVELS_INSTRUCTION);
-	// });
-	// let welcomeScreen = $state(true);
+	import { welcomeScreen } from '$stores/app';
 </script>
 
-<!-- <div class="tab_tab-pane">
-	<div class="tab_content-component">
-		<div class="tab_content-left">
-			<div class="margin-bottom margin-small">
-				<h3 class="tab_heading">
-					Ogólne informacje <br /> o ruchu <br /> "Wolność i Pokój"
-				</h3>
-			</div>
-			<p class="text-size-medium">
-				Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem
-				próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając
-				praktycznie niezmienionym.
-				<span class="text-weight-bold"
-					>Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem
-					próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym,
-					pozostając praktycznie niezmienionym.
-				</span>
-			</p>
-		</div> -->
-<section class="w-full max-w-2xl overflow-y-hidden">
-	<div class="padding-global">
-		<div class="container-large">
-			<div class="">
-				<!-- padding-section-large -->
-				<!-- padding-section-large -->
-				<!-- <div class="padding-global"> -->
-				<div class="container-large overflow-y-hidden">
-					{#if $welcomeScreen}
-						<div class="flex flex-col content-center items-center justify-center" transition:slide>
-							<div class="margin-bottom margin-small">
-								<h3 class="tab_heading text-center">
-									Witamy w przewodniku <br /> po ruchu <br class="hidden-sm" />
-									<span>
-										"Wolność i Pokój" <img
-											src="/images/dove-icon_black.svg"
-											class="hidden-sm -ml-2 h-20 w-14 pb-2"
-											alt=""
-										/></span
-									>
-								</h3>
-							</div>
-							<p class="text-size-medium mb-10">
-								Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia
-								tekstem próbnej książki. Pięć wieków później zaczął być używany.
-								<span class="text-weight-bold"
-									>Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia
-									tekstem próbnej książki.
-								</span>
-							</p>
-
-							<div class="grid grid-cols-2 gap-x-8 px-4">
-								<a href="/" target="_blank" class="button is-secondary"
-									><div class="button-text">Główna</div>
-									<div class="button-background"></div>
-								</a>
-
-								<div class="">
-									<button onclick={() => ($welcomeScreen = false)} class="button is-icon is-primary"
-										><div class="button-text button-next-slide"></div>
-										<div class="button-background"></div>
-									</button>
-								</div>
-
-								<!-- <button class="button is-primary"
-									><div class="button-text">
-										<span class="hidden sm:inline">Rozpocząć</span>
-										<span class="inline sm:hidden">&lt;</span>
-									</div>
-									<div class="button-background"></div>
-								</button> -->
-							</div>
-						</div>
-					{:else}
-						<div class="" transition:slide>
-							<ul
-								aria-label="Accordion Control Group Buttons"
-								role="list"
-								class="tab_content-list w-list-unstyled w-full"
-							>
-								{#each appScreensMenu as mode}
-									<li class="tab_list-item w-full">
-										<div class="flex w-full flex-row justify-between">
-											<div role="region" class="tab_item w-full">
-												<div class="tab_item-title">
-													<h4 class="heading-style-h6">{mode.title}</h4>
-												</div>
-												<div class="tab_item-content">
-													<div>
-														<span class="hidden-sm">progres</span> <span>{mode.progress}</span> /
-														<span>{mode.data.length}</span>
-													</div>
-												</div>
-											</div>
-											<div class="p-2">
-												<button
-													onclick={() => {
-														$selectedPlayMenu = mode.id;
-														$selectedAppMode = mode;
-														$selectedMenu = 1;
-													}}
-													class="button is-small is-icon is-primary"
-													><div class="">
-														&gt;
-														<!-- <span class="hidden sm:inline">Rozpocząć</span> -->
-														<!-- <span class="inline button-text sm:hidden">">"</span> -->
-													</div>
-													<div class="button-background"></div>
-												</button>
-											</div>
-										</div>
-									</li>
-								{/each}
-							</ul>
-
-							<!-- <button
-						onclick={() => ($welcomeScreen = false)}
-						class="button is-secondary mx-auto mt-8 max-w-sm"
-						><div class="button-text">
-							<span class="button-text mr-2">&lt;&lt;</span> Z powrotem
-						</div>
-						<div class="button-background"></div>
-					</button> -->
-						</div>
-					{/if}
-				</div>
-			</div>
-		</div>
+<div class="flex flex-col content-center items-center justify-center" transition:slide>
+	<div class="margin-bottom margin-small">
+		<h3 class="tab_heading text-center">
+			Witamy w przewodniku <br /> po ruchu <br class="hidden-sm" />
+			<span>
+				"Wolność i Pokój" <img
+					src="/images/dove-icon_black.svg"
+					class="hidden-sm -ml-2 h-20 w-14 pb-2"
+					alt=""
+				/></span
+			>
+		</h3>
 	</div>
-</section>
+	<p class="text-size-medium mb-10">
+		Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej
+		książki. Pięć wieków później zaczął być używany.
+		<span class="text-weight-bold"
+			>Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej
+			książki.
+		</span>
+	</p>
 
-<!-- </div>
-</div> -->
+	<div class="grid grid-cols-2 gap-x-8 px-4">
+		<a href="/" target="_blank" class="button is-secondary"
+			><div class="button-text">Główna</div>
+			<div class="button-background"></div>
+		</a>
+
+		<button onclick={() => ($welcomeScreen = false)} class="button is-icon is-primary"
+			><div class="button-text button-next-slide"></div>
+			<div class="button-background"></div>
+		</button>
+	</div>
+</div>
 
 <style lang="postcss">
 	.button-next-slide {
