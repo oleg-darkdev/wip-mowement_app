2dca4ba26146b89f29070d61d75870df14276047
Bear-software <linner_952@mail.ru>
2025-04-25 16:59:55 +0200

    alfa v.

---
+++ src/lib/shared/data/productslIst.ts
@@ -0,0 +1,54 @@
+let products = {
+		promo: [
+			{
+				title: 'lorem',
+				code: 'lorem',
+				complexity: 'lorem',
+				img: '/images/about_3.jpg'
+			},
+			{
+				title: 'lorem',
+				code: 'lorem',
+				complexity: 'lorem',
+				img: '/images/about_3.jpg'
+			},
+			{
+				title: 'lorem',
+				code: 'lorem',
+				complexity: 'lorem',
+				img: '/images/about_3.jpg'
+			},
+			{
+				title: 'lorem',
+				code: 'lorem',
+				complexity: 'lorem',
+				img: '/images/about_3.jpg'
+			},
+			{
+				title: 'lorem',
+				code: 'lorem',
+				complexity: 'lorem',
+				img: '/images/about_3.jpg'
+			},
+			{
+				title: 'lorem',
+				code: 'lorem',
+				complexity: 'lorem',
+				img: '/images/about_3.jpg'
+			},
+			{
+				title: 'lorem',
+				code: 'lorem',
+				complexity: 'lorem',
+				img: '/images/about_3.jpg'
+			},
+			{
+				title: 'lorem',
+				code: 'lorem',
+				complexity: 'lorem',
+				img: '/images/about_3.jpg'
+			}
+		]
+	};
+
+export default products;
