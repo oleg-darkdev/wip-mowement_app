03d41a0aa1a3fbc7361826e537cd0b802a87b07b
Bear-software <linner_952@mail.ru>
2025-04-25 16:59:31 +0200

    alfa v.

---
+++ src/lib/widgets/app/Cart.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+	// import {  } from '$shared';
+	// import {  } from '$widgets';
+	// import {  } from '$entities'
+</script>
+
+
+<div class="cart-modal">
+  <div scroll="enable" data-w-id="255d5b0b-33cf-f30e-7ac5-00733b1141dc" class="cart-blend"></div>
+  <div id="cart-modal" class="cart-wrapper">
+    <div class="cart-inner-wrapper">
+      <div class="big-heading has__all-caps cc-cart-heading">Warenkorb</div>
+      <div id="cart-item-wrapper" class="cart-item-wrapper">
+        <div id="cart-empty-wrapper" class="cart-empty-wrapper">
+          <div class="cart-empty-text">Dein Korb ist leer. Wie wärs denn hiermit? :)</div>
+          <div class="cart-suggestion-wrapper w-dyn-list">
+            <div role="list" class="cart-suggestion-list w-dyn-items">
+              <div role="listitem" class="cart-suggestion-item w-dyn-item">
+                <a href="/" class="link-block-product cc-cart-suggestion w-inline-block">
+                  <div class="product-meta-wrapper">
+                    <div class="product-heading-wrapper">
+                      <h3 class="cart-item-overview-heading">ÄTHIOPIEN light roast</h3>
+                      <div class="organic-logo w-condition-invisible">
+                        <img src="/images/613c8a9ba868284649a34016_EU-Organic-Logo.svg" loading="lazy" alt="EU-Bio-Logo. Vorverpackte Bio-Lebensmittel, aus der EU, welche die strengen Normen der EU-Rechtsvorschriften für den ökologischen Landbau erfüllen, müssen seit dem 1. Juli 2010 verpflichtend mit dem EU-Bio-Logo gekennzeichnet werden." class="organic-logo-eu">
+                      </div>
+                    </div>
+                    <div class="product-overview-price-wrapper">
+                      <div class="product-overview-price-holder main-price">
+                        <div price="ÄTHIOPIEN light roast" class="product-overview-price">12.00</div>
+                        <div class="product-overview-price">€</div>
+                      </div>
+                      <div class="product-overview-price-holder sold-out w-condition-invisible">
+                        <div fs-cmssort-field="price" class="product-overview-price">sold out</div>
+                      </div>
+                      <div class="product-overview-price-holder off-price w-condition-invisible">
+                        <div price="ÄTHIOPIEN light roast" class="product-overview-price off-price w-dyn-bind-empty"></div>
+                        <div class="product-overview-price">€</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="product-thumbnail-wrapper">
+                    <img src="/images/6386341915e596443954002a_AethiopienLaenderkaffee_KROeMKaffeeroestereiMagdeburg.jpeg" loading="lazy" alt="ÄTHIOPIEN light roast" class="product-image cc-landingpage">
+                  </div>
+                </a>
+              </div>
+              <div role="listitem" class="cart-suggestion-item w-dyn-item">
+                <a href="/" class="link-block-product cc-cart-suggestion w-inline-block">
+                  <div class="product-meta-wrapper">
+                    <div class="product-heading-wrapper">
+                      <h3 class="cart-item-overview-heading">DECAF - ent­koffe­inier­ter Kaffee</h3>
+                      <div class="organic-logo w-condition-invisible">
+                        <img src="/images/613c8a9ba868284649a34016_EU-Organic-Logo.svg" loading="lazy" alt="EU-Bio-Logo. Vorverpackte Bio-Lebensmittel, aus der EU, welche die strengen Normen der EU-Rechtsvorschriften für den ökologischen Landbau erfüllen, müssen seit dem 1. Juli 2010 verpflichtend mit dem EU-Bio-Logo gekennzeichnet werden." class="organic-logo-eu">
+                      </div>
+                    </div>
+                    <div class="product-overview-price-wrapper">
+                      <div class="product-overview-price-holder main-price">
+                        <div price="DECAF - ent­koffe­inier­ter Kaffee" class="product-overview-price">10.00</div>
+                        <div class="product-overview-price">€</div>
+                      </div>
+                      <div class="product-overview-price-holder sold-out w-condition-invisible">
+                        <div fs-cmssort-field="price" class="product-overview-price">sold out</div>
+                      </div>
+                      <div class="product-overview-price-holder off-price w-condition-invisible">
+                        <div price="DECAF - ent­koffe­inier­ter Kaffee" class="product-overview-price off-price w-dyn-bind-empty"></div>
+                        <div class="product-overview-price">€</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="product-thumbnail-wrapper">
+                    <img src="/images/65d4dbabb8725cba75e1bd79_DECAF_-_KROM_Kaffeerosterei_Magdeburg.jpeg" loading="lazy" alt="DECAF - ent­koffe­inier­ter Kaffee" sizes="100vw" srcset="/images/65d4dbabb8725cba75e1bd79_DECAF_-_KROM_Kaffeerosterei_Magdeburg-p-500.jpeg 500w, /images/65d4dbabb8725cba75e1bd79_DECAF_-_KROM_Kaffeerosterei_Magdeburg-p-800.jpeg 800w, /images/65d4dbabb8725cba75e1bd79_DECAF_-_KROM_Kaffeerosterei_Magdeburg-p-1080.jpeg 1080w, /images/65d4dbabb8725cba75e1bd79_DECAF_-_KROM_Kaffeerosterei_Magdeburg.jpeg 1200w" class="product-image cc-landingpage">
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="cart-item-loading-spinner" class="cart-item-loading-spinner">
+          <div class="loading-spinner" data-w-id="7530af3d-e416-203f-25d3-3207bbc9251b" data-animation-type="lottie" data-src="https:/cdn.prod.website-files.com/610cf33140aaf753c6f8f7bb/62695a031f1563936b525a72_lf30_editor_3sebc4ky.json" data-loop="1" data-direction="1" data-autoplay="1" data-is-ix2-target="0" data-renderer="svg" data-default-duration="3.16" data-duration="0"></div>
+        </div>
+      </div>
+      <div class="cart-summary-wrapper">
+        <div class="cart-price-wrapper">
+          <div class="cart-price">Zwischensumme:</div>
+          <div id="cart-price" class="cart-price">00,00€</div>
+        </div>
+        <div class="checkout-info">Versand und Rabattcodes werden beim Checkout hinzugefügt.</div>
+        <div id="cart-checkout-button" class="std-button is__black is__bigger is__100 is__not-active">Zur Kasse</div>
+      </div>
+      <div scroll="enable" data-w-id="255d5b0b-33cf-f30e-7ac5-00733b1141e8" class="close-cart-modal-button">
+        <div class="x-stroke rotated"></div>
+        <div class="x-stroke"></div>
+      </div>
+      <div id="cart-preloader" class="cart-preloader">
+        <div class="div-block-32">
+          <div data-w-id="d3da09da-f961-afd7-dc20-602f410e8f5d" data-animation-type="lottie" data-src="https:/cdn.prod.website-files.com/610cf33140aaf753c6f8f7bb/61965ab806482f517e990003_herdkanne%20-%20login.json" data-loop="1" data-direction="1" data-autoplay="1" data-is-ix2-target="0" data-renderer="svg" data-default-duration="5.005004801146985" data-duration="0"></div>
+          <div class="normal-text is__bold">Gleich gehts weiter zur Kasse</div>
+        </div>
+      </div>
+      <div class="cart-maintence-block">
+        <div class="margin-bottom margin-small">
+          <div class="semi-heading">Wir sind gleich zurück 🚧</div>
+        </div>
+        <div class="normal-text">Hey du! Sorry! Aktuell führen wir Wartungsarbeiten an der Seite durch. Schau bitte später nochmal vorbei wenn wir durch sind :-). Vielen Dank für dein Verständnis! KRÖM</div>
+      </div>
+    </div>
+  </div>
+  <div class="cart-background"></div>
+</div>
+
+
+<style lang="postcss">
+</style>
---
+++ src/lib/widgets/landing/Stats.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { StatCard } from '$sharedUi';
+	import { stats } from '$sharedData';
+	// import {  } from '$entities'
+
+	
+</script>
+
+<div id="" class="tab_component w-tabs">
+	<div class="tab_tabs-content w-tab-content">
+		<div class="tab_tab-pane">
+			<div class="kr-m-values-layout">
+				{#each stats as stat}
+					<StatCard {stat} />
+				{/each}
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.kr-m-values-layout {
+		grid-column-gap: 16px;
+		grid-row-gap: 16px;
+		grid-template-rows: auto;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		grid-auto-columns: 1fr;
+		align-items: start;
+		display: grid;
+	}
+
+	@media screen and (max-width: 767px) {
+		.kr-m-values-layout {
+			grid-template-rows: auto auto;
+			grid-template-columns: 1fr 1fr;
+		}
+	}
+
+	@media screen and (max-width: 479px) {
+		.kr-m-values-layout {
+			grid-column-gap: 0px;
+			grid-row-gap: 0px;
+			grid-template-rows: auto auto;
+			grid-template-columns: 1fr 1fr;
+			margin-right: -1px;
+		}
+	}
+</style>
