00ed644cb1bba981ad793bc91779244f38d96531
Bear-software <linner_952@mail.ru>
2025-04-25 17:00:35 +0200

    fix max height

--- src/lib/widgets/app/Footer.svelte
+++ src/lib/widgets/app/Footer.svelte
@@ -6,8 +6,8 @@
 	// import { slide } from 'svelte/transition'; transition:slide
 </script>
 
-<footer  class="background-color-gray fixed bottom-0 left-0 w-full">
-	<div class="mx-auto w-full max-w-4xl pb-6">
+<footer  class="max-h-32 background-color-gray fixed bottom-0 left-0 w-full">
+	<div class="mx-auto w-full max-w-4xl lg:pb-6 pb-1">
 		<div class="mx-auto w-full max-w-lg">
 			<div class="mx-auto flex justify-center gap-x-4">
 				<slot />
